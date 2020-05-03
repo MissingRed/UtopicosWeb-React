19fadde4ec53a620d142d0091f966eb2e5752473
Juan David <gottygarcia@gmail.com>
2020-05-03 17:53:14 -0500

    Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,48 +1,50 @@
-import React, { useCallback, useContext } from "react";
-import { withRouter, Redirect } from "react-router";
+import React, { useCallback, useContext } from 'react'
+import { withRouter, Redirect } from 'react-router'
+import '../styles/login.css'
 
-import app from "../Database/base.js";
-import { AuthContext } from "../Database/Auth.js";
+import app from '../Database/base.js'
+import { AuthContext } from '../Database/Auth.js'
 
 const Login = ({ history }) => {
-  const handleLogin = useCallback(
-    async (event) => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await app
-          .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
-        history.push("/");
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+	const handleLogin = useCallback(
+		async (event) => {
+			event.preventDefault()
+			const { email, password } = event.target.elements
+			try {
+				await app.auth().signInWithEmailAndPassword(email.value, password.value)
+				history.push('/')
+			} catch (error) {
+				alert(error)
+			}
+		},
+		[history]
+	)
 
-  const { currentUser } = useContext(AuthContext);
+	const { currentUser } = useContext(AuthContext)
 
-  if (currentUser) {
-    return <Redirect to="/" />;
-  }
+	if (currentUser) {
+		return <Redirect to="/" />
+	}
 
-  return (
-    <div>
-      <h1>Iniciar Sesión</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Correo
-          <input name="email" type="email" />
-        </label>
-        <label>
-          Contraseña
-          <input name="password" type="password" />
-        </label>
-        <button type="submit">Iniciar Sesion</button>
-      </form>
-    </div>
-  );
-};
+	return (
+		<div className="login-container">
+			<div>
+				<img src="/img/reducido.png" alt="Utópicos" />
+				<span>Inicia sesión para ingresar al panel administrador</span>
+				<form onSubmit={handleLogin}>
+					<input placeholder="Correo" name="email" type="email" />
+					<input
+						placeholder="Contraseña"
+						name="password"
+						className="line-input"
+						type="password"
+					/>
+					<button type="submit">Iniciar Sesión</button>
+				</form>
+			</div>
+			<img src="/img/bobble.svg" className="bobble" alt="" />
+		</div>
+	)
+}
 
-export default withRouter(Login);
+export default withRouter(Login)
