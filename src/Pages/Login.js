e8ed77f6b256674a6dcb60b1fc108ca75b6208ce
daniel <rodriguezdaniel048@gmail.com>
2020-05-04 13:06:15 -0500

    Version 3 - Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,51 +1,54 @@
-import React, { useCallback, useContext } from 'react'
-import { withRouter, Redirect } from 'react-router'
-import '../styles/login.css'
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import "../styles/login.css";
 
-import app from '../Database/base.js'
-import { AuthContext } from '../Database/Auth.js'
+import app from "../Database/base.js";
+import { AuthContext } from "../Database/Auth.js";
 
 const Login = ({ history }) => {
-	const handleLogin = useCallback(
-		async (event) => {
-			event.preventDefault()
-			const { email, password } = event.target.elements
-			try {
-				await app.auth().signInWithEmailAndPassword(email.value, password.value)
-				history.push('/')
-			} catch (error) {
-				alert(error)
-			}
-		},
-		[history]
-	)
+  const handleLogin = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .signInWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
 
-	const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
 
   if (currentUser) {
     return <Redirect to="/home" />;
   }
 
+  return (
+    <div className="login-container">
+      <div>
+        <img src="/Img/reducido.png" alt="Utópicos" />
+        <span>Inicia sesión para ingresar al panel administrador</span>
+        <form onSubmit={handleLogin}>
+          <input placeholder="Correo" name="email" type="email" />
+          <input
+            placeholder="Contraseña"
+            name="password"
+            className="line-input"
+            type="password"
+          />
+          <button type="submit">
+            <img src="Img/arrow.svg" alt="Ir" />
+          </button>
+        </form>
+      </div>
+      <img src="/img/bobble.svg" className="bobble" alt="" />
+    </div>
+  );
+};
 
-	return (
-		<div className="login-container">
-			<div>
-				<img src="/img/reducido.png" alt="Utópicos" />
-				<span>Inicia sesión para ingresar al panel administrador</span>
-				<form onSubmit={handleLogin}>
-					<input placeholder="Correo" name="email" type="email" />
-					<input
-						placeholder="Contraseña"
-						name="password"
-						className="line-input"
-						type="password"
-					/>
-					<button type="submit">Iniciar Sesión</button>
-				</form>
-			</div>
-			<img src="/img/bobble.svg" className="bobble" alt="" />
-		</div>
-	)
-}
-
-export default withRouter(Login)
+export default withRouter(Login);
